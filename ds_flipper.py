ea27a5b060686ad3476a82bf1ed7923292881b1a
mpierangeli <mpierangeli@estudiantes.unsam.edu.ar>
2022-10-22 19:57:48 -0300

    pruebas con ds

--- ds_flipper.py
+++ ds_flipper.py
@@ -1,12 +1,12 @@
 import os
 import nibabel as nib
 import numpy as np
-import matplotlib.pyplot as plt
 
-fp = r"C:\Users\Pier\Desktop\PFI_main\ds_mezcla_segmentacion integra\finales2"
-fp2 = r"C:\Users\Pier\Desktop\PFI_main\ds_mezcla_segmentacion integra\flipped"
+fp = r"C:\Users\Pier\Desktop\PFI_main\ds_mezcla_segmentacion integra\nuevas2"
+fp2 = r"C:\Users\Pier\Desktop\PFI_main\ds_mezcla_segmentacion integra\nuevas"
 allfiles = os.listdir(fp)
 for file in sorted(allfiles):
+    print(file)
     x = nib.load(os.path.join(fp,file))
     img = x.get_fdata()
     img = np.rot90(img,3)
